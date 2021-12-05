85b798804541c40fe3b72a07eb27d6680b51d0f8
Ruhul <ruhul98@gmail.com>
2021-12-05 11:08:53 +0530

    first commit

---
+++ src/test/java/com/ruhul/betterreadsapp/BetterReadsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ruhul.betterreadsapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BetterReadsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
